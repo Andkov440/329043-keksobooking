b2e9fd9cc4647a49f728c6eeb13425f5de93d251
Kovalev Andrey <andkov440@mail.ru>
2018-02-06 17:53:29 +0300

    Сделал 2 задания

--- js/map.js
+++ js/map.js
@@ -1,11 +1,22 @@
 'use strict';
 
-var HOUSE_TITLE = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var HOUSE_TITLE = ['Большая уютная квартира',
+  'Маленькая неуютная квартира',
+  'Огромный прекрасный дворец',
+  'Маленький ужасный дворец',
+  'Красивый гостевой домик',
+  'Некрасивый негостеприимный домик',
+  'Уютное бунгало далеко от моря',
+  'Неуютное бунгало по колено в воде'];
+
 var ADS_QUANTITY = 8;
 var TIME = ['12:00', '13:00', '14:00'];
 var HOUSE_TYPE = ['flat', 'house', 'bungalo'];
 var FEATURES_ITEMS = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 var similarAds = [];
 
@@ -31,14 +42,46 @@ var mixArray = function (arr) {
 };
 
 var generateArrayRandomLength = function (arr) {
-  return mixArray(arr).slice(0, randomValue(arr.length - 1));
+  var newArr = arr.slice();
+  return mixArray(newArr).slice(0, randomValue(newArr.length - 1));
 };
 
 var createAdsData = function (adsArray) {
   for (var i = 0; i < ADS_QUANTITY; i++) {
-    adsArray.push({author: {avatar: 'img/avatars/user0' + (i + 1) + '.png'}, offer: {title: HOUSE_TITLE[i], address: 'location.x, location.y', price: randomValueFromRange(1000, 1000000), type: HOUSE_TYPE[randomValue(HOUSE_TYPE.length)], rooms: randomValueFromRange(1, 5), guests: randomValueFromRange(1, 10), checkin: TIME[randomValue(TIME.length)], checkout: TIME[randomValue(TIME.length)], features: generateArrayRandomLength(FEATURES_ITEMS), description: '', photos: mixArray(PHOTOS)}, location: {x: randomValueFromRange(300, 900), y: randomValueFromRange(150, 500)}});
+    var pinLocation = {
+      x: randomValueFromRange(300, 900),
+      y: randomValueFromRange(150, 500)
+    };
+    adsArray.push({
+      author: {avatar: 'img/avatars/user0' + (i + 1) + '.png'},
+      offer: {title: HOUSE_TITLE[i],
+        address: pinLocation.x + ',' + pinLocation.y,
+        price: randomValueFromRange(1000, 1000000),
+        type: HOUSE_TYPE[randomValue(HOUSE_TYPE.length)],
+        rooms: randomValueFromRange(1, 5),
+        guests: randomValueFromRange(1, 10),
+        checkin: TIME[randomValue(TIME.length)],
+        checkout: TIME[randomValue(TIME.length)],
+        features: generateArrayRandomLength(FEATURES_ITEMS),
+        description: '',
+        photos: mixArray(PHOTOS)},
+      location: pinLocation
+    });
   }
   return adsArray;
 };
 
+console.log(createAdsData(similarAds));
+
 document.querySelector('.map').classList.remove('map--faded');
+
+var mapPins = document.querySelector('.map__pins');
+var fragment = document.createDocumentFragment();
+
+for (var i = 0; i < ADS_QUANTITY; i++) {
+ var newPin = document.createElement('button');
+ newPin.className = 'map__pin';
+ newPin.style = 'left: ' + location.x + 'px; top: ' + location.y + 'px';
+ newPin.innerHTML = '<img src="' + author.avatar + '" width="40" height="40" draggable="false">';
+}
+console.log(newPin);
