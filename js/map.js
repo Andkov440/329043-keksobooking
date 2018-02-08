c84f341b47777a4cf57adc6c6af55c6c73d3258f
Kovalev Andrey <andkov440@mail.ru>
2018-02-08 11:34:33 +0300

    Исправил замечания

--- js/map.js
+++ js/map.js
@@ -21,7 +21,7 @@ var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
 var PIN_WIDTH = 40;
 var PIN_HEIGHT = 70;
 
-var similarAds = [];
+
 
 var generateValueFromRange = function (min, max) {
   var rand = min + Math.random() * (max + 1 - min);
@@ -51,19 +51,20 @@ var removeChildren = function (elem) {
   }
 };
 
-var createAdsData = function (adsArray) {
+var createadverts = function () {
+  var similarAds = [];
   for (var i = 0; i < ADS_QUANTITY; i++) {
     var pinLocation = {
       x: generateValueFromRange(300, 900),
       y: generateValueFromRange(150, 500)
     };
-    adsArray.push({
+    similarAds.push({
       author: {
         avatar: 'img/avatars/user0' + (i + 1) + '.png'
       },
       offer: {
         title: HOUSE_TITLES[i],
-        address: pinLocation.x + PIN_WIDTH / 2 + ',' + pinLocation.y + PIN_HEIGHT,
+        address: pinLocation.x + ',' + pinLocation.y,
         price: generateValueFromRange(1000, 1000000),
         type: Object.keys(HOUSE_TYPES)[generateValueFromRange(0, Object.keys(HOUSE_TYPES).length - 1)],
         rooms: generateValueFromRange(1, 5),
@@ -77,50 +78,50 @@ var createAdsData = function (adsArray) {
       location: pinLocation
     });
   }
-  return adsArray;
+  return similarAds;
 };
 
 document.querySelector('.map').classList.remove('map--faded');
-var adsData = createAdsData(similarAds);
+var adverts = createadverts();
 
-var generatePins = function (adsArray) {
+var generatePins = function (ads) {
   var mapPins = document.querySelector('.map__pins');
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < ADS_QUANTITY; i++) {
     var newPin = document.createElement('button');
     newPin.className = 'map__pin';
-    newPin.style = 'left: ' + adsArray[i].location.x + 'px; top: ' + adsArray[i].location.y + 'px';
-    newPin.innerHTML = '<img src="' + adsArray[i].author.avatar + '" width="40" height="40" draggable="false">';
+    newPin.style = 'left: ' + ads[i].location.x + PIN_WIDTH / 2 + 'px; top: ' + ads[i].location.y + PIN_HEIGHT + 'px';
+    newPin.innerHTML = '<img src="' + ads[i].author.avatar + '" width="40" height="40" draggable="false">';
     fragment.appendChild(newPin);
   }
   mapPins.appendChild(fragment);
 };
 
-var createAdvert = function (adsArray) {
+var createAdvert = function (ads) {
   var adsTemplateElement = document.querySelector('template').content;
   var mapFiltersContainer = document.querySelector('.map__filters-container');
-  adsTemplateElement.querySelector('h3').textContent = adsArray[0].offer.title;
-  adsTemplateElement.querySelector('small').textContent = adsArray[0].offer.address;
-  adsTemplateElement.querySelector('.popup__price').textContent = adsArray[0].offer.price + '&#x20bd;/ночь';
-  adsTemplateElement.querySelector('h4').textContent = HOUSE_TYPES[adsArray[0].offer.type];
-  adsTemplateElement.querySelector('p:nth-child(7)').textContent = adsArray[0].offer.rooms + ' комнаты для ' + adsArray[0].offer.guests + ' гостей';
-  adsTemplateElement.querySelector('p:nth-child(8)').textContent = 'Заезд после ' + adsArray[0].offer.checkin + ', выезд до ' + adsArray[0].offer.checkout;
-  adsTemplateElement.querySelector('p:last-of-type').textContent = adsArray[0].offer.description;
-  adsTemplateElement.querySelector('.popup__avatar').src = adsArray[0].author.avatar;
+  adsTemplateElement.querySelector('h3').textContent = ads.offer.title;
+  adsTemplateElement.querySelector('small').textContent = ads.offer.address;
+  adsTemplateElement.querySelector('.popup__price').textContent = ads.offer.price + ' &#x20bd;/ночь';
+  adsTemplateElement.querySelector('h4').textContent = HOUSE_TYPES[ads.offer.type];
+  adsTemplateElement.querySelector('p:nth-child(7)').textContent = ads.offer.rooms + ' комнаты для ' + ads.offer.guests + ' гостей';
+  adsTemplateElement.querySelector('p:nth-child(8)').textContent = 'Заезд после ' + ads.offer.checkin + ', выезд до ' + ads.offer.checkout;
+  adsTemplateElement.querySelector('p:last-of-type').textContent = ads.offer.description;
+  adsTemplateElement.querySelector('.popup__avatar').src = ads.author.avatar;
 
   document.querySelector('.map').insertBefore(adsTemplateElement, mapFiltersContainer);
 
   removeChildren(document.querySelector('.popup__features'));
-  for (var i = 0; i < adsArray[0].offer.features.length; i++) {
-    document.querySelector('.popup__features').innerHTML += '<li class="feature feature--' + adsArray[0].offer.features[i] + '"></li>';
+  for (var i = 0; i < ads.offer.features.length; i++) {
+    document.querySelector('.popup__features').innerHTML += '<li class="feature feature--' + ads.offer.features[i] + '"></li>';
   }
 
   removeChildren(document.querySelector('.popup__pictures'));
-  for (i = 0; i < adsArray[0].offer.photos.length; i++) {
-    document.querySelector('.popup__pictures').innerHTML += '<li><img src="' + adsArray[0].offer.photos[i] + '" width="50" height="50"></li>';
+  for (i = 0; i < ads.offer.photos.length; i++) {
+    document.querySelector('.popup__pictures').innerHTML += '<li><img src="' + ads.offer.photos[i] + '" width="50" height="50"></li>';
   }
 };
 
-generatePins(adsData);
-createAdvert(adsData);
+generatePins(adverts);
+createAdvert(adverts[0]);
 
